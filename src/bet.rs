a502536062b9614f5cf155d0eae08581cb62f19f
Paulo.Moreira <paulocandango@gmail.com>
2024-10-23 09:25:33 -0300

    Exibindo a fatura ligthning e exibindo uma imagem preta aleatoria no desenho do QrCode, preparando para gerar o QrCode contendo a fatura

--- src/bet.rs
+++ src/bet.rs
@@ -5,6 +5,9 @@ use tera::{Tera, Context};
 use crate::{parse_numbers, BetForm};
 use reqwest::Client;
 use serde_json::Value;
+use base64::encode;
+use std::io::Cursor;
+use image::{ImageBuffer, Rgba, RgbaImage, ImageFormat};
 
 // Controller para a rota /bet
 pub async fn bet(tmpl: web::Data<Tera>) -> impl Responder {
@@ -24,6 +27,7 @@ pub async fn bet(tmpl: web::Data<Tera>) -> impl Responder {
 
 // Controller para a rota /placeBet
 pub async fn place_bet(tmpl: web::Data<Tera>, form: web::Form<BetForm>) -> impl Responder {
+
     println!("--- Registrando Aposta ---");
     println!("Loteria: {}", form.lottery);
     println!("Carteira Bitcoin: {}", form.wallet);
@@ -42,12 +46,15 @@ pub async fn place_bet(tmpl: web::Data<Tera>, form: web::Form<BetForm>) -> impl 
         }
     };
 
+    let qr_code_base64 = generate_qr_code_base64();
+
     let mut context = Context::new();
     context.insert("lottery", &form.lottery);
     context.insert("wallet", &form.wallet);
     context.insert("numbers", &form.numbers);
     context.insert("balance", &formatted_balance);
-    context.insert("qrcode", &qr_code); // Inserindo o QR Code no contexto
+    context.insert("qrcode", &qr_code);
+    context.insert("qr_code_base64", &qr_code_base64);
 
     let rendered = tmpl.render("placebet.html", &context).unwrap();
 
@@ -97,3 +104,20 @@ async fn create_invoice(amount: i64, memo: &str) -> Result<String, reqwest::Erro
     Ok(response["payment_request"].as_str().unwrap_or("").to_string())
 }
 
+//------------------------------------------------------------------------------------------
+pub fn generate_qr_code_base64() -> String {
+    // Cria uma imagem 100x100 com um quadrado preto simples
+    let img: RgbaImage = ImageBuffer::from_pixel(100, 100, Rgba([0, 0, 0, 255]));
+
+    // Cria um buffer para armazenar a imagem PNG
+    let mut buffer = Cursor::new(Vec::new());
+
+    // Salva a imagem no formato PNG dentro do buffer
+    img.write_to(&mut buffer, ImageFormat::Png).unwrap();
+
+    // Converte o conteúdo do buffer em uma string Base64
+    let base64_string = encode(buffer.get_ref());
+
+    // Retorna a imagem como uma data URI para ser usada no HTML
+    format!("data:image/png;base64,{}", base64_string)
+}
